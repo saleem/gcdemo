3518acebf1cb77fdce814bf31b5db953741099ad
Saleem Siddiqui <saleem@saleem-siddiqui.com>
2015-06-27 12:18:41 +0200

    Removed commented out code

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         WeakReference<Person> personWeakReference = new WeakReference<>(new Person("John"));
         WeakReference<Apartment> apartmentWeakReference = new WeakReference<>(new Apartment(73));
-//        personWeakReference = null;
-//        apartmentWeakReference = null;
         System.gc();
         Thread.sleep(500);
     }
