68fd74705f5134068076f4eed9d04e268c275c51
Saleem Siddiqui <saleem@saleem-siddiqui.com>
2018-05-23 18:34:22 -0500

    Unit tests for RationalNumber

--- src/main/java/com/test/generics/RationalNumber.java
+++ src/main/java/com/test/generics/RationalNumber.java
@@ -4,7 +4,13 @@ import java.math.*;
 import java.util.*;
 
 public class RationalNumber extends RealNumber {
+    public static final int DEFAULT_SCALE = 0;
+
     public RationalNumber(BigInteger numerator, BigInteger denominator) {
-        super(new BigDecimal(Objects.requireNonNull(numerator)).divide(new BigDecimal(Objects.requireNonNull(denominator))));
+        this(numerator, denominator, DEFAULT_SCALE);
+    }
+
+    public RationalNumber(BigInteger numerator, BigInteger denominator, int scale) {
+        super(new BigDecimal(Objects.requireNonNull(numerator)).divide(new BigDecimal(Objects.requireNonNull(denominator)), scale, BigDecimal.ROUND_HALF_EVEN));
     }
 }
--- src/test/java/com/test/generics/NumbersTest.java
+++ src/test/java/com/test/generics/NumbersTest.java
@@ -10,11 +10,16 @@ import static org.hamcrest.core.Is.is;
 public class NumbersTest {
     @DisplayName("Tests for our domain-specific Integer class")
     @Test public void testInteger() {
-        assertThat(new Integer(new BigInteger("0")).toString(), is("ComplexNumber{realPart=0, imaginaryPart=0}"));
+        assertThat(new Integer(BigInteger.ZERO).toString(), is("ComplexNumber{realPart=0, imaginaryPart=0}"));
         assertThat(new Integer(new BigInteger("42")).toString(), is("ComplexNumber{realPart=42, imaginaryPart=0}"));
         assertThat(new Integer(new BigInteger("1234567890987654321")).toString(), is("ComplexNumber{realPart=1234567890987654321, imaginaryPart=0}"));
         assertThat(new Integer(new BigInteger("-1234567890987654321")).toString(), is("ComplexNumber{realPart=-1234567890987654321, imaginaryPart=0}"));
     }
 
-
+    @DisplayName("Tests for our domain-specific RationalNumber class")
+    @Test public void testRationalNumber() {
+        assertThat(new RationalNumber(BigInteger.ZERO, BigInteger.ONE).toString(), is("ComplexNumber{realPart=0, imaginaryPart=0}"));
+        assertThat(new RationalNumber(BigInteger.ONE, new BigInteger("42"), 20).toString(), is("ComplexNumber{realPart=0.02380952380952380952, imaginaryPart=0}"));
+        assertThat(new RationalNumber(BigInteger.ONE, new BigInteger("-42"), 20).toString(), is("ComplexNumber{realPart=-0.02380952380952380952, imaginaryPart=0}"));
+    }
 }
