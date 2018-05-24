4a8617160e003b34a096a681895e2eaf27a7aabe
Saleem Siddiqui <saleem@saleem-siddiqui.com>
2018-05-23 20:12:28 -0500

    Unit tests for equivalence of objects of different Number types

--- src/test/java/com/test/generics/GenericsTest.java
+++ src/test/java/com/test/generics/GenericsTest.java
@@ -3,35 +3,22 @@ package com.test.generics;
 import java.math.*;
 import org.junit.jupiter.api.*;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.core.Is.is;
 
 @DisplayName("Test cases to demonstrate how generics work")
 public class GenericsTest {
     @DisplayName("Verify that is-a relationship works as expected for number types")
-    @Test public void testIsARelationship() {
-        ComplexNumber complexNumber = new ComplexNumber(new BigDecimal("42"), BigDecimal.ZERO);
-        RealNumber realNumber = new RealNumber(new BigDecimal("42"));
+    @Test
+    public void testIsARelationship() {
+        ComplexNumber complexNumber = new ComplexNumber(BigDecimal.TEN, BigDecimal.ONE);
+        RealNumber realNumber = new RealNumber(new BigDecimal("42.123456789"));
         RationalNumber rationalNumber = new RationalNumber(new BigInteger("420"), BigInteger.TEN);
-        Integer integer = new Integer(new BigInteger("42"));
+        Integer integer = new Integer(new BigInteger("42000"));
 
-        // twelve assertions of equality; both directions for each pair of numbers
-        assertThat(complexNumber, is(realNumber));
-        assertThat(complexNumber, is(rationalNumber));
-        assertThat(complexNumber, is(integer));
-
-        assertThat(realNumber, is(rationalNumber));
-        assertThat(realNumber, is(integer));
-
-        assertThat(rationalNumber, is(integer));
-
-        assertThat(realNumber, is(complexNumber));
-        assertThat(rationalNumber, is(complexNumber));
-        assertThat(integer, is(complexNumber));
-
-        assertThat(rationalNumber, is(realNumber));
-        assertThat(integer, is(realNumber));
-
-        assertThat(integer, is(rationalNumber));
+        assertThat(complexNumber, instanceOf(ComplexNumber.class));
+        assertThat(realNumber, instanceOf(ComplexNumber.class));
+        assertThat(rationalNumber, instanceOf(ComplexNumber.class));
+        assertThat(integer, instanceOf(ComplexNumber.class));
     }
 }
--- src/test/java/com/test/generics/NumbersTest.java
+++ src/test/java/com/test/generics/NumbersTest.java
@@ -33,4 +33,31 @@ public class NumbersTest {
     @Test public void testComplexNumber() {
         assertThat(new ComplexNumber(BigDecimal.TEN, BigDecimal.ONE).toString(), is("ComplexNumber{realPart=10, imaginaryPart=1}"));
     }
+
+    @DisplayName("Tests for equality of the values of various numbers, regardless of the type / class of Number")
+    @Test void testEquality() {
+        ComplexNumber complexNumber = new ComplexNumber(new BigDecimal("42"), BigDecimal.ZERO);
+        RealNumber realNumber = new RealNumber(new BigDecimal("42"));
+        RationalNumber rationalNumber = new RationalNumber(new BigInteger("420"), BigInteger.TEN);
+        Integer integer = new Integer(new BigInteger("42"));
+
+        // twelve assertions of equality; both directions for each pair of numbers
+        assertThat(complexNumber, is(realNumber));
+        assertThat(complexNumber, is(rationalNumber));
+        assertThat(complexNumber, is(integer));
+
+        assertThat(realNumber, is(rationalNumber));
+        assertThat(realNumber, is(integer));
+
+        assertThat(rationalNumber, is(integer));
+
+        assertThat(realNumber, is(complexNumber));
+        assertThat(rationalNumber, is(complexNumber));
+        assertThat(integer, is(complexNumber));
+
+        assertThat(rationalNumber, is(realNumber));
+        assertThat(integer, is(realNumber));
+
+        assertThat(integer, is(rationalNumber));
+    }
 }
